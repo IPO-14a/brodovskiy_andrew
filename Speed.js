c68a33daec20b2b9c61164e6beb99e4009ecf1e7
Mushr0om <shroomyhs@gmail.com>
2017-05-30 22:00:04 +0300

    Add function "cl"

--- Speed.js
+++ Speed.js
@@ -1,2 +1,11 @@
 (function (count,fileds) {
+    function cl(id){x=document.getElementById(id);return x?(x.className.indexOf('bomb')!=-1?1:0):0;}
+    var bombs=0;
+    for(i=0;i<fileds;i++){
+        r=document.createElement('div');
+        if(Math.random()*fileds<count){r.className='bomb close',document.getElementById('text').innerHTML=(++bombs)+' bomb\'s';}
+        else r.className='close';
+        r.id=Math.floor(i/10)+'_'+i%10;
+        document.body.appendChild(r);
+    }
 }(10,100));

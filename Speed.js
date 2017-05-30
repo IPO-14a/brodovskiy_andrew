0d1e1b6e28905757877e3499b3c166c3b60a5441
Mushr0om <shroomyhs@gmail.com>
2017-05-30 22:03:44 +0300

    Add function "open"

--- Speed.js
+++ Speed.js
@@ -8,4 +8,12 @@
         r.id=Math.floor(i/10)+'_'+i%10;
         document.body.appendChild(r);
     }
+    function open(i,j){
+        dom=document.getElementById(i+'_'+j);
+        if(!dom||dom.className.indexOf('close')==-1)return;
+        if(dom.className.indexOf('bomb')!=-1){
+            divs=document.getElementsByTagName('div');
+            for(i=0;i<divs.length;i++)divs[i].className=divs[i].className.indexOf('bomb')!=-1?'bomb':'';
+            alert('You lose!');
+        }
 }(10,100));

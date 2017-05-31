4123297b835d3718f7db89694d37b1c73360f6f9
Mushr0om <shroomyhs@gmail.com>
2017-05-31 09:28:48 +0300

    Fixed standards

--- Speed.js
+++ Speed.js
@@ -8,13 +8,17 @@
         r = document.createElement('div');
         if (Math.random() * fileds < count) {
             r.className = 'bomb_close', document.getElementById('text').innerHTML = (++bombs) + ' bomb\'s';
-        } else r.className = 'close';
+        } else {
+            r.className = 'close';
+        }
         r.id = Math.floor(i / 10) + '_' + i % 10;
         document.body.appendChild(r);
     }
     for (o = 0; o < fileds; o++) {
         i = Math.floor(o / 10), j = o % 10, num = 0, obj = document.getElementById(i + '_' + j);
-        for (k = 0; k < 9; k++) num += cl((i - (Math.floor(k / 3) - 1)) + '_' + (j - (k % 3 - 1)));
+        for (k = 0; k < 9; k++) {
+            num += cl((i - (Math.floor(k / 3) - 1)) + '_' + (j - (k % 3 - 1)));
+        }
         obj.innerHTML = num == 0 ? ' ' : num;
         obj.onclick = function() {
             mix = this.id.split('_'), open(mix[0], mix[1]);
@@ -25,21 +29,33 @@
         }
         function open(i, j) {
             dom = document.getElementById(i + '_' + j);
-            if (!dom || dom.className.indexOf('close') == -1) return;
+            if (!dom || dom.className.indexOf('close') == -1) {
+                return;
+            }
             if (dom.className.indexOf('bomb') != -1) {
                 divs = document.getElementsByTagName('div');
-                for (i = 0; i < divs.length; i++) divs[i].className = divs[i].className.indexOf('bomb') != -1 ? 'bomb' : '';
+                for (i = 0; i < divs.length; i++) {
+                    divs[i].className = divs[i].className.indexOf('bomb') != -1 ? 'bomb' : '';
                 alert('You lose!');
+                }
             } else {
                 dom.className = '';
                 var elems = document.getElementsByTagName('div'),
                     len = 0;
-                for (ki in elems)
-                    if (elems[ki].className && elems[ki].className.indexOf('close') != -1) len++;
-                if (len <= bombs) alert('You win!'); //low code and allerts
+                for (ki in elems) {
+                    if (elems[ki].className && elems[ki].className.indexOf('close') != -1) }
+                    len++;
+                }
+                if (len <= bombs) {
+                    alert('You win!');
+                }
+                }
             }
-            if (dom.innerHTML == ' ')
-                for (var ks = 0; ks < 9; ks++) open(i - ((Math.floor(ks / 3) - 1)), j - (((ks % 3) - 1)));
+            if (dom.innerHTML == ' ') {
+                for (var ks = 0; ks < 9; ks++) {
+                    open(i - ((Math.floor(ks / 3) - 1)), j - (((ks % 3) - 1)));
+                }
+            }
         }
     }
 }(10, 100));

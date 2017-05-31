1795546af85bfb713089012a8e9ff52442cd1a72
Mushr0om <shroomyhs@gmail.com>
2017-05-31 07:54:44 +0300

    error

--- Speed.js
+++ Speed.js
@@ -7,7 +7,7 @@
     for (i = 0; i < fileds; i++) {
         r = document.createElement('div');
         if (Math.random() * fileds < count) {
-            r.className = 'bomb close', document.getElementById('text').innerHTML = (++bombs) + ' bomb\'s';
+            r.className = 'bomb_close', document.getElementById('text').innerHTML = (++bombs) + ' bomb\'s';
         } else r.className = 'close';
         r.id = Math.floor(i / 10) + '_' + i % 10;
         document.body.appendChild(r);
